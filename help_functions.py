236ee2f486a7f9b01e41c3dbfac8de466d885141
wildan94 <danielsson16@gmail.com>
2017-11-19 09:54:14 +0100

    1

---
+++ help_functions.py
@@ -0,0 +1,35 @@
+
+
+##
+# Gini coefficient
+def gini(solution, submission):
+    df = zip(solution, submission, range(len(solution)))
+    df = sorted(df, key=lambda x: (x[1],-x[2]), reverse=True)
+    rand = [float(i+1)/float(len(df)) for i in range(len(df))]
+    totalPos = float(sum([x[0] for x in df]))
+    cumPosFound = [df[0][0]]
+    for i in range(1,len(df)):
+        cumPosFound.append(float(cumPosFound[len(cumPosFound)-1] + df[i][0]))
+    Lorentz = [float(x)/totalPos for x in cumPosFound]
+    Gini = [Lorentz[i]-rand[i] for i in range(len(df))]
+    return sum(Gini)
+##
+# Normalized Gini coefficient
+def normalized_gini(solution, submission):
+    normalized_gini = gini(solution, submission)/gini(solution, solution)
+    return normalized_gini
+
+##
+# K-Fold with gini
+def k_fold_gini(X_train, y_train,n_splits=2, shuffle=False, random_state=3, clf_function=XGBClassifier()):
+    kf=KFold(n_splits=n_splits, shuffle=shuffle, random_state=random_state)
+    gini_score=[]
+
+    for train_index, test_index in kf.split(X_train):
+        clf=clone(clf_function)
+        clf.fit(X_train[train_index], y_train[train_index])
+        y_pred=clf.predict_proba(X_train[test_index])[:,1]
+        gini_score.append(normalized_gini(y_train[test_index], y_pred))
+    mean_gini=np.mean(gini_score)
+    return "Mean-normalized_gini: %s"%mean_gini
+
